64172a3a703541d0d5d10bee1d239574412802c1
Naveenbittu <85916508+Naveenbittu@users.noreply.github.com>
2021-06-21 19:33:37 +0530

    Add files via upload

---
+++ multipliction_matrix.c
@@ -0,0 +1,49 @@
+/**1. Write a C Program to show matrix multiplication on 2-D Array**/
+#include <stdio.h>
+#define N 4
+
+
+void multiply(int mat1[][N], int mat2[][N], int res[][N])
+{
+    int i, j, k;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            res[i][j] = 0;
+            for (k = 0; k < N; k++)
+                res[i][j] += mat1[i][k] * mat2[k][j];
+        }
+    }
+}
+
+int main()
+{
+    int mat1[N][N] = { { 1, 1, 1, 1 },
+                       { 2, 2, 2, 2 },
+                       { 3, 3, 3, 3 },
+                       { 4, 4, 4, 4 } };
+
+    int mat2[N][N] = { { 1, 1, 1, 1 },
+                       { 2, 2, 2, 2 },
+                       { 3, 3, 3, 3 },
+                       { 4, 4, 4, 4 } };
+
+    int res[N][N]; // To store result
+    int i, j;
+    multiply(mat1, mat2, res);
+
+    printf("Result matrix is \n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++)
+            printf("%d ", res[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
+#if 0
+Result matrix is
+10 10 10 10
+20 20 20 20
+30 30 30 30
+40 40 40 40
+#endif // 0
